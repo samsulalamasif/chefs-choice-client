37ab2d6fbaf86f91688f8861409f4c4f9480afff
samsulalamasif <samsulalamasif@gmail.com>
2023-05-02 03:23:38 +0600

    client sied router setup with tailwind

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            <h1 className='text-5xl text-center'>This is main</h1>
+        </div>
+    );
+};
+
+export default Main;
